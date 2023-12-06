680dc7af4dd87472b213d9e8dbfce7a47b8d6246
411748 <411748@bsd48.org>
2023-12-05 19:00:34 -0800

    second version for the assignment

---
+++ node.h
@@ -0,0 +1,24 @@
+//Nick Braun
+//12/5/2023
+//Linked List Part 1
+#ifndef NODE_H
+#define NODE_H
+
+#include <iostream>
+#include <cstring>
+#include "student.h"
+
+using namespace std;
+
+class Node {
+ public:
+  Node(Student*); //Constructor
+  ~Node(); //Deconstructor
+  Node* getNext(); //Gets next node pointer
+  void setNext(Node* newnext); //Sets the next pointer to the corresponding node pointer
+  Student* getStudent(); //Get student pointer
+ private:
+  Student* student;
+  Node* next;
+};
+#endif
